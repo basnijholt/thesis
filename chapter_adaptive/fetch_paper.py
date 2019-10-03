2b686eec340476ed491342ef190103cdbb440633
Bas Nijholt <basnijholt@gmail.com>
2019-10-03 11:37:23 +0200

    improve fetch_paper.py

--- chapter_adaptive/fetch_paper.py
+++ chapter_adaptive/fetch_paper.py
@@ -1,27 +1,37 @@
 import os
+import shutil
 
-import wget
+folder = "../../adaptive-paper/"
+for old, new in (
+    ("paper.yaml", "adaptive.yaml"),
+    ("paper.tex", "adaptive_full.tex"),
+    ("not_on_crossref.bib", "not_on_crossref.bib"),
+):
+    shutil.copyfile(os.path.join(folder, old), new)
 
-url = "https://gitlab.kwant-project.org/qt/adaptive-paper/builds/artifacts/master/raw/paper.tex?job=make"
-os.system(f"wget -O adaptive_full.tex  {url}")
+start = r"\section{Introduction}"
+end = r"\section*{Acknowledgements}"
+
+to_replace = [
+    (r"\citep", r"\cite"),
+    (r"\(", r"$"),
+    (r"\)", r"$"),
+    (r"\includegraphics{figures/", r"\includegraphics{chapter_adaptive/figures/"),
+    (r"\onlinecite{", r"\cite{"),
+    (r"\paragraph", r"\co"),
+]
 
 with open("adaptive_full.tex") as f:
     add = False
     text = []
     for line in f.readlines():
-        if add is False and r"\hypertarget{introduction}" in line:
+        if add is False and start in line:
             add = True
-        if add is True and r"\section*{Acknowledgements}" in line:
+        if add is True and end in line:
             add = False
         if add:
-            line = line.replace(r"\citep", r"\cite")
-            line = line.replace(r"\(", r"$")
-            line = line.replace(r"\)", r"$")
-            line = line.replace(
-                r"\includegraphics{figures/",
-                r"\includegraphics{chapter_adaptive/figures/",
-            )
-            line = line.replace(r"\onlinecite{", r"\cite{")
+            for old, new in to_replace:
+                line = line.replace(old, new)
             text.append(line)
 text = "".join(text)
 
@@ -40,23 +50,5 @@ tex = (
 with open("adaptive.tex", "w") as f:
     f.writelines(tex)
 
-
-os.system("rm -fr tmp* figures adaptive-paper-master-figures")
-os.system(
-    "wget -O tmp.zip https://gitlab.kwant-project.org/qt/adaptive-paper/-/archive/master/adaptive-paper-master.zip?path=figures"
-)
-os.system("unzip tmp.zip")
-os.system("mv adaptive-paper-master-figures/figures .")
-os.system("rm -fr adaptive-paper-master-figures")
-
-fnames = {
-    "paper.yaml": "adaptive.yaml",
-    "paper.bib": "adaptive.bib",
-    "not_on_crossref.bib": "not_on_crossref.bib",
-}
-for fname_old, fname_new in fnames.items():
-    os.system(f"rm -fr {fname_old}")
-    cmd = f"wget -O {fname_old} https://gitlab.kwant-project.org/qt/adaptive-paper/raw/master/{fname_old}?inline=false"
-    print(cmd)
-    os.system(cmd)
-    os.rename(fname_old, fname_new)
+old, new = (os.path.join(folder, "figures"), os.path.abspath("figures"))
+os.system(f"rm -fr {new}; cp -r {old} {new}")
