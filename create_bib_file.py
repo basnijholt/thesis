27ac9d38a0127f2b4621cad36c3a2459b19366e3
Bas Nijholt <basnijholt@gmail.com>
2019-11-04 16:35:58 +0100

    add page numbers correctly

--- create_bib_file.py
+++ create_bib_file.py
@@ -1,3 +1,4 @@
+import contextlib
 import functools
 import glob
 import os
@@ -6,10 +7,83 @@ from concurrent.futures import ThreadPoolExecutor
 import diskcache
 import requests
 import yaml
-import tqdm
+from crossref.restful import Etiquette, Works
+from tqdm import tqdm
+
+works = Works(etiquette=Etiquette("publist", contact_email="basnijholt@gmail.com"))
 
 
-def replace_key(key, bib_entry):
+JOURNALS = [
+    ("Advanced Materials", "Adv. Mater."),
+    ("Annals of Physics", "Ann. Phys."),
+    ("Applied Physics Letters", "Appl. Phys. Lett."),
+    ("JETP Lett", "JETP Lett."),
+    ("Journal de Physique", "J. Phys."),
+    ("Journal of Computational Physics", "J. Comput. Phys."),
+    ("Journal of Experimental and Theoretical Physics", "J. Exp. Theor. Phys."),
+    ("Journal of Low Temperature Physics", "J. Low Temp. Phys."),
+    (
+        "Journal of Physics A: Mathematical and Theoretical",
+        "J. Phys. A: Math. Theor.",
+    ),
+    ("Journal of Physics: Condensed Matter", "J. Phys.: Condens. Matter"),
+    ("Nano Letters", "Nano Lett."),
+    ("Nature Communications", "Nat. Commun."),
+    ("Nature Materials", "Nat. Mater."),
+    ("Nature Nanotechnology", "Nat. Nanotechnol."),
+    ("Nature Physics", "Nat. Phys."),
+    ("New Journal of Physics", "New J. Phys."),
+    ("Physical Review B", "Phys. Rev. B"),
+    ("Physical Review Letters", "Phys. Rev. Lett."),
+    ("Physical Review X", "Phys. Rev. X"),
+    ("Physical Review", "Phys. Rev."),  # should be before the above subs
+    ("Physics-Uspekhi", "Phys. Usp."),
+    ("Reports on Progress in Physics", "Rep. Prog. Phys."),
+    ("Review of Scientific Instruments", "Rev. Sci. Instrum."),
+    ("Reviews of Modern Physics", "Rev. Mod. Phys."),
+    ("Science Advances", "Sci. Adv."),
+    ("Scientific Reports", "Sci. Rep."),
+    ("Semiconductor Science and Technology", "Semicond. Sci. Technol."),
+    (
+        "Annual Review of Condensed Matter Physics",
+        "Annu. Rev. Condens. Matter Phys.",
+    ),
+    ("{EPL} (Europhysics Letters)", "{EPL}"),
+    ("Nature Reviews Materials", "Nat. Rev. Mater."),
+    ("Physics Letters", "Phys. Lett."),
+    ("The European Physical Journal B", "Eur. Phys. J. B"),
+    ("{SIAM} Journal on Numerical Analysis", "{SIAM} J. Numer. Anal."),
+    ("{AIP} Conference Proceedings", "{AIP} Conf. Proc."),
+]
+
+
+def get_pages(data, works=works):
+    try:
+        page = data["article-number"]
+    except KeyError:
+        if "page" in data:
+            page = data["page"].split("-")[0]
+        else:
+            raise Exception('No page number found!')
+    return page
+
+
+def get_journal(data, works=works):
+    return data["container-title"][0], data["short-container-title"][0]
+
+
+def cached_crossref(doi):
+    """Look up if this has previously been called."""
+    with diskcache.Cache("crossref.pickle") as cache:
+        info = cache.get(doi)
+        if info is not None:
+            return info
+        info = works.doi(doi)
+        cache[doi] = info
+        return info
+
+
+def replace_key(key, data, bib_entry):
     bib_type, *_ = bib_entry.split("{")
     _, *rest = bib_entry.split(",")
     rest = ",".join(rest)
@@ -48,60 +122,36 @@ def replace_key(key, bib_entry):
         ),  # fix for 10.1103/physrevlett.98.010506
     ]
 
-    # I got these by using JabRef and converting to abbr journals
-    # and parsing the git diff.
-    journals = [
-        ("Advanced Materials", "Adv. Mater."),
-        ("Annals of Physics", "Ann. Phys."),
-        ("Applied Physics Letters", "Appl. Phys. Lett."),
-        ("JETP Lett", "JETP Lett."),
-        ("Journal de Physique", "J. Phys."),
-        ("Journal of Computational Physics", "J. Comput. Phys."),
-        ("Journal of Experimental and Theoretical Physics", "J. Exp. Theor. Phys."),
-        ("Journal of Low Temperature Physics", "J. Low Temp. Phys."),
-        (
-            "Journal of Physics A: Mathematical and Theoretical",
-            "J. Phys. A: Math. Theor.",
-        ),
-        ("Journal of Physics: Condensed Matter", "J. Phys.: Condens. Matter"),
-        ("Nano Letters", "Nano Lett."),
-        ("Nature Communications", "Nat. Commun."),
-        ("Nature Materials", "Nat. Mater."),
-        ("Nature Nanotechnology", "Nat. Nanotechnol."),
-        ("Nature Physics", "Nat. Phys."),
-        ("New Journal of Physics", "New J. Phys."),
-        ("Physical Review B", "Phys. Rev. B"),
-        ("Physical Review Letters", "Phys. Rev. Lett."),
-        ("Physical Review X", "Phys. Rev. X"),
-        ("Physical Review", "Phys. Rev."),  # should be before the above subs
-        ("Physics-Uspekhi", "Phys. Usp."),
-        ("Reports on Progress in Physics", "Rep. Prog. Phys."),
-        ("Review of Scientific Instruments", "Rev. Sci. Instrum."),
-        ("Reviews of Modern Physics", "Rev. Mod. Phys."),
-        ("Science Advances", "Sci. Adv."),
-        ("Scientific Reports", "Sci. Rep."),
-        ("Semiconductor Science and Technology", "Semicond. Sci. Technol."),
-    ]
+    to_replace += JOURNALS  # hard coded abbrvs.
 
-    # Manually added
-    journals += [
-        (
-            "Annual Review of Condensed Matter Physics",
-            "Annu. Rev. Condens. Matter Phys.",
-        ),
-        ("{EPL} (Europhysics Letters)", "{EPL}"),
-        ("Nature Reviews Materials", "Nat. Rev. Mater."),
-        ("Physics Letters", "Phys. Lett."),
-        ("The European Physical Journal B", "Eur. Phys. J. B"),
-        ("{SIAM} Journal on Numerical Analysis", "{SIAM} J. Numer. Anal."),
-        ("{AIP} Conference Proceedings", "{AIP} Conf. Proc."),
-    ]
+    with contextlib.suppress(Exception):
+        # Use the journal abbrv.
+        to_replace.append(get_journal(data))
 
-    for old, new in to_replace + journals:
+    for old, new in to_replace:
         rest = rest.replace(old, new)
 
     result = bib_type + "{" + key + "," + rest
+
+    if "pages = {" not in result:
+        # Add the page number if it's missing
+        with contextlib.suppress(Exception):
+            pages = get_pages(data)
+            lines = result.split('\n')
+            lines.insert(2, f'\tpages = {{{pages}}},')
+            result = "\n".join(lines)
+
     return result
+
+
+def doi2bib(doi):
+    """Return a bibTeX string of metadata for a given DOI."""
+    print(f"Requesting {doi}")
+    url = "http://dx.doi.org/" + doi
+    headers = {"accept": "application/x-bibtex"}
+    r = requests.get(url, headers=headers)
+    r.encoding = "utf-8"
+    return r.text
 
 
 def cached_doi2bib(doi):
@@ -117,45 +167,32 @@ def cached_doi2bib(doi):
         return text
 
 
-def doi2bib(doi):
-    """Return a bibTeX string of metadata for a given DOI."""
-    print(f"Requesting {doi}")
-    url = "http://dx.doi.org/" + doi
-    headers = {"accept": "application/x-bibtex"}
-    r = requests.get(url, headers=headers)
-    r.encoding = "utf-8"
-    return r.text
+if __name__ == "__main__":
+    bibs = [f for f in glob.glob("*/*yaml") if "tmp.yaml" not in f]
+    print("Reading: ", bibs)
 
+    mapping = {}
+    for fname in bibs:
+        with open(fname) as f:
+            mapping = {**mapping, **yaml.safe_load(f)}
+    dois = dict(sorted(mapping.items()))
 
-bibs = [f for f in glob.glob("*/*yaml") if "tmp.yaml" not in f]
-print("Reading: ", bibs)
+    entries = [
+        replace_key(key, data=cached_crossref(doi), bib_entry=cached_doi2bib(doi))
+        for key, doi in tqdm(dois.items())
+    ]
 
-mapping = {}
-for fname in bibs:
-    with open(fname) as f:
-        mapping = {**mapping, **yaml.safe_load(f)}
-dois = dict(sorted(mapping.items()))
-
-# Doing it in parallel doesn't seem to work since ~13 Oct
-# with ThreadPoolExecutor() as ex:
-#     futs = ex.map(cached_doi2bib, list(dois.values()))
-#     bibs = list(futs)
-
-bibs = [cached_doi2bib(doi) for doi in tqdm.tqdm(dois.values())]
-entries = [replace_key(key, bib) for key, bib in zip(dois.keys(), bibs)]
-
-
-bib_files = glob.glob("chapter_*/not_on_crossref.bib")
-with open("dissertation.bib", "w") as outfile:
-    outfile.write("@preamble{ {\\providecommand{\\BIBYu}{Yu} } }\n\n")
-    for fname in bib_files:
-        outfile.write(f"\n% Below is from `{fname}`.\n\n")
-        with open(fname) as infile:
-            outfile.write(infile.read())
-    outfile.write("\n% Below is from all `yaml` files.\n\n")
-    for e in entries:
-        for line in e.split("\n"):
-            # Remove the url line
-            if "url = {" not in line:
-                outfile.write(f"{line}\n")
-        outfile.write("\n")
+    bib_files = glob.glob("chapter_*/not_on_crossref.bib")
+    with open("dissertation.bib", "w") as outfile:
+        outfile.write("@preamble{ {\\providecommand{\\BIBYu}{Yu} } }\n\n")
+        for fname in bib_files:
+            outfile.write(f"\n% Below is from `{fname}`.\n\n")
+            with open(fname) as infile:
+                outfile.write(infile.read())
+        outfile.write("\n% Below is from all `yaml` files.\n\n")
+        for e in entries:
+            for line in e.split("\n"):
+                # Remove the url line
+                if "url = {" not in line:
+                    outfile.write(f"{line}\n")
+            outfile.write("\n")
